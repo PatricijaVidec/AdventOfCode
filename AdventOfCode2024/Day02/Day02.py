ac70da61d09588e41f08d12eb4d3fcc387d3c308
Patricija Videc <patricijavidec1@gmail.com>
2024-12-02 19:18:14 +0100

    Completed the second part of the second day of the AoC.

--- AdventOfCode2024/Day02/Day02.py
+++ AdventOfCode2024/Day02/Day02.py
@@ -20,13 +20,42 @@ def safe_report(report):
 
     return (increasing or decreasing) and valid
 
+
 def count_safe_reports():
-    # lastly I upen up the file, I sum up all the save files, and apply the criteria I set up in the function above
+    # lastly I open up the file, I sum up all the save files, and apply the criteria I set up in the function above
     with open('input.txt') as file:
         return sum(safe_report(line.strip()) for line in file if line.strip())
+
 
 # I print out the result of the safe reports
 print(count_safe_reports())
 
 
 # PART 2
+def safe_report2(levels):
+    # checking if a report is safe
+    differences = [levels[i+1] - levels[i] for i in range(len(levels) - 1)]
+    return (all(1 <= abs(d) <= 3 for d in differences)  # between 1 and 3 (inclusive), returns true if it is
+            and (all(d > 0 for d in differences)  # check is positive
+            or all(d < 0 for d in differences)))  # checks if negative
+
+# it can only contain either negatives or positives ^^
+
+
+def can_be_safe(report):
+    # checking if a report can be made safe by removing one level
+    levels = list(map(int, report.split()))
+    for i in range(len(levels)):
+        modified_levels = levels[:i] + levels[i+1:]  # remove the level at index i
+        if safe_report2(modified_levels):  # checking if anything changed by calling the func safe_report2
+            return True
+    return False
+
+
+def count_safe_reports2():
+    with open('input.txt') as file:
+        data = [line.strip() for line in file if line.strip()]
+    return sum(safe_report2(list(map(int, report.split()))) or can_be_safe(report) for report in data)
+
+
+print(count_safe_reports2())
